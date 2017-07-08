99c67454a7f08296b8052b93cf3be88e8091b733
MyCloudDoor <javier.serra@myclouddoor.com>
2017-07-08 22:55:21 +0200

    Add files via upload

---
+++ UNIX/LeerDesdeFichero.sh
@@ -0,0 +1,13 @@
+                  ##############################################################################################################################
+                  #                                        ReadFromFile.sh                                                                     #
+                  # Written for MyCloudDoor for the post https://github.com/MyCloudDoor/UnixScripting.gitlinux-server-health-monitoring-script/#
+                  # If any bug, report us in the link below                                                                                    #
+                  # Free to use/edit/distribute the code below by                                                                              #
+                  # giving proper credit to MyCloudDoor.com                                                                                    #
+                  #                                                                                                                            #
+                  ##############################################################################################################################
+#!/bin/bash
+SUM='cat InputFile | wc -l'
+for A in 'seq 1 $SUM' ; do
+head -$A InputFile | tail -1
+done
