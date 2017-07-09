76f1a8140ae51edfb52d0df03495a1ece15e5b88
MyCloudDoor <javier.serra@myclouddoor.com>
2017-07-10 00:55:00 +0200

    Update Monitor_UnixSystem.sh

--- UNIX/Monitor_UnixSystem.sh
+++ UNIX/Monitor_UnixSystem.sh
@@ -29,7 +29,7 @@ then
 wd=$(pwd)
 basename "$(test -L "$0" && readlink "$0" || echo "$0")" > /tmp/scriptname
 scriptname=$(echo -e -n $wd/ && cat /tmp/scriptname)
-su -c "cp $scriptname /usr/bin/monitor" root && echo "Congratulations! Script Installed, now run monitor Command" || echo "Installation failed"
+su -c "cp $scriptname /usr/bin/monitor" root && echo "Script Installed, now run monitor Command" || echo "Installation failed"
 }
 fi
 
